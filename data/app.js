b03bec09b44832363566b80d97af9aa00ca8651d
Bradyk927 <Bradyk927@gmail.com>
2018-08-14 08:45:00 -0700

    p

---
+++ data/app.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+      name: 'Brady King',
+      profilePic: '',
+      scores: [
+        "1",
+        "5",
+        "1",
+        "5",
+        "1",
+        "5",
+        "1",
+        "5",
+        "1",
+        "5"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
